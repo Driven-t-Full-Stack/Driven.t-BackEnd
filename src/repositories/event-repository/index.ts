14d389cd47850e2a7f662b56d46bed0cbeffb80e
andreprogramador01 <andreprogramador01@gmail.com>
2023-05-18 15:30:27 -0300

    feat: redis on repository event

--- src/repositories/event-repository/index.ts
+++ src/repositories/event-repository/index.ts
@@ -1,7 +1,23 @@
 import { prisma } from '@/config';
+import { redis } from '@/config/redis';
+
+const cacheKey = 'event';
 
 async function findFirst() {
-  return prisma.event.findFirst();
+  const cachedEvent = await redis.get(cacheKey);
+
+  if (cachedEvent) {
+    const event = JSON.parse(cachedEvent);
+    return event;
+  }
+  const event = await prisma.event.findFirst();
+
+  delete event.createdAt;
+  delete event.updatedAt;
+
+  redis.set(cacheKey, JSON.stringify(event));
+
+  return event;
 }
 
 const eventRepository = {
